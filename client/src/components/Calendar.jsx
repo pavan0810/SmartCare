b825be9ca5d1c8e21165f94e7a0b11c1a91c782e
pavan0810 <PH526@live.mdx.ac.uk>
2025-05-29 22:01:57 +0400

    Populating dictionary to store appointments and adding calendar component

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,34 @@
+import Fullcalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import timeGridPlugin from '@fullcalendar/timegrid'
+import interactionPlugin from '@fullcalendar/interaction'
+
+export default function Calendar() {
+    let appointments = {}
+    let allAppointments = [
+        { title: 'event 1', date: '2025-04-01' },
+        { title: 'event 2', date: '2025-04-02' }
+    ]
+
+    // function to format dates
+    function formatDate(date) {
+        return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+    }
+
+    let startDate = new Date(2025,4,29)
+    let endDate = new Date(2025, 11, 31)
+    for(let date = new Date(startDate); date <= endDate;date.setDate(date.getDate() + 1)) {
+        appointments[formatDate(date)] = new Array(5)
+    }
+    console.log(appointments)
+    let date = new Date(2025, 0, 20)
+    console.log(formatDate(date))
+
+    
+    return(
+        <Fullcalendar 
+        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+        initialView={'dayGridMonth'}
+        events={allAppointments}/>        
+    )
+}
--- client/src/index.js
+++ client/src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Header from './components/Header'
+import Calendar from './components/Calendar'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
     <Header />
+    <Calendar />
   </React.StrictMode>
 );
 
