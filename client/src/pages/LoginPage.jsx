9e8f659cb8268537af12289dcbf2c78dd265d700
pavan0810 <PH526@live.mdx.ac.uk>
2025-06-30 23:31:03 +0400

    added css to login page

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from "react-router-dom"
+import '../css/loginPage.css'
 export default function LoginPage({ setUser }) {
     const navigate  = useNavigate();
     const [ username , setUsername] = useState('');
@@ -33,12 +34,15 @@ export default function LoginPage({ setUser }) {
     }
 
     return(
-        <div>
-            <div>
-                <h1>SmartCare</h1>
+        <div className="login">
+            <div className="loginLogo">
+                <div>
+                    <h1>SmartCare</h1>
+                    <img src='images/logo.png' alt='SmartCare logo'/> 
+                </div>
             </div>
-            <div>
-                <h3>Welcome to SmartCare!</h3>
+            <div className="loginForm">
+                <h2>Welcome to SmartCare!</h2>
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="username">Username</label>
                     <input onChange={handleUsernameChange} type="text" name="username" id="username" required/>
